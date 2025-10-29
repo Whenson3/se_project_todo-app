9dc7d8e8dd08726907313e79cfae88e4ab440851
Whenson3 <Whenson3@yahoo.com>
2025-10-29 09:25:11 -0500

    New update for my project

--- components/Todo.js
+++ components/Todo.js
@@ -21,23 +21,39 @@ class Todo {
        this._todoLabel.setAttribute("for", "todo-${this._data.id}");
     }
 
+    generateDueDate() {
+      this._dueDate = new Date(this._data.date);
+      if (!isNaN(this._dueDate)) {
+        this._todoDate.textContent = `Due: ${this._dueDate.toLocaleString("en-US", {
+          year: "numeric",
+          month: "short",
+          day: "numeric",
+        })}`;
+      }
+    }
+
     getView() {
       this._todoElement = this._templateElement.content
         .querySelector(".todo")
         .cloneNode(true);
 
       const todoNameEl = this._todoElement.querySelector(".todo__name");
-      const todoDate = this._todoElement.querySelector(".todo__date");
+      this._todoDate = this._todoElement.querySelector(".todo__date");
       this._todoDeleteBtn = this._todoElement.querySelector(".todo__delete-btn");
 
       todoNameEl.textContent = this._data.name;
+
+  
+    }
+      }
       
       this.generateCheckboxEl();
       this._setEventListeners();
+      this.generateDueDate();
       
 
       return this._todoElement;
-    }
-}
+    
+
 
 export default Todo;
