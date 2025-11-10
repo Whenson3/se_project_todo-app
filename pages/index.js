4bdfc3e6d8f97529bea94e3940513f70ae5d9df1
Whenson3 <Whenson3@yahoo.com>
2025-11-10 10:50:18 -0600

    New update for my project

--- pages/index.js
+++ pages/index.js
@@ -3,62 +3,56 @@ import { v4 as uuidv4 } from 'https://jspm.dev/uuid';
 import { initialTodos, validationConfig} from "../utils/constant.js";
 import Todo from "../components/Todo.js";
 import FormValidator from "../components/FormValidator.js";
+import Section from '../components/Section.js';
 
 const addTodoButton = document.querySelector(".button_action_add");
 const addTodoPopup = document.querySelector("#add-todo-popup");
 const addTodoForm = addTodoPopup.querySelector(".popup__form");
 const addTodoCloseBtn = addTodoPopup.querySelector(".popup__close");
-// const todoTemplate = document.querySelector("#todo-template"); => removed as it's now in Todo.js
-const todosList = document.querySelector(".todos__list");
+
+const section = new Section({
+  items: initialTodos,
+  renderer: (item) => {
+    const todoElement = generateTodo(item);
+    section.addItem(todoElement);
+  },
+  containerSelector: ".todos__list",
+});
+
+section.renderItems();
 
 const openModal = (modal) => {
+  if (!modal) return;
+  
+  const handleEscClose = (evt) => {
+    if (evt.key === "Escape") {
+      closeModal(modal);
+    }
+  };
+
   modal.classList.add("popup_visible");
+  document.addEventListener("keydown", handleEscClose);
+  
+  // Store the handler reference for cleanup
+  modal._handleEscClose = handleEscClose;
 };
 
 const closeModal = (modal) => {
+  if (!modal) return;
+  
   modal.classList.remove("popup_visible");
+  
+  // Remove escape key listener
+  if (modal._handleEscClose) {
+    document.removeEventListener("keydown", modal._handleEscClose);
+    delete modal._handleEscClose;
+  }
 };
 
-// The logic in this function should all be handled in the Todo class.
 const generateTodo = (data) => {
   const todo = new Todo(data, "#todo-template");
   const todoElement = todo.getView();
   return todoElement;
-
-  // To Be Removed:
-//   const todoElement = todoTemplate.content
-//     .querySelector(".todo")
-//     .cloneNode(true);
-//   const todoNameEl = todoElement.querySelector(".todo__name");
-//   const todoCheckboxEl = todoElement.querySelector(".todo__completed");
-//   const todoLabel = todoElement.querySelector(".todo__label");
-//   const todoDate = todoElement.querySelector(".todo__date");
-//   const todoDeleteBtn = todoElement.querySelector(".todo__delete-btn");
-
-//   todoNameEl.textContent = data.name;
-//   todoCheckboxEl.checked = data.completed;
-
-//   // Apply id and for attributes.
-//   // The id will initially be undefined for new todos.
-//   todoCheckboxEl.id = `todo-${data.id}`;
-//   todoLabel.setAttribute("for", `todo-${data.id}`);
-
-//   // If a due date has been set, parsing this it with `new Date` will return a
-//   // number. If so, we display a string version of the due date in the todo.
-//   const dueDate = new Date(data.date);
-//   if (!isNaN(dueDate)) {
-//     todoDate.textContent = `Due: ${dueDate.toLocaleString("en-US", {
-//       year: "numeric",
-//       month: "short",
-//       day: "numeric",
-//     })}`;
-//   }
-
-//   todoDeleteBtn.addEventListener("click", () => {
-//     todoElement.remove();
-//   });
-
-  
 };
 
 addTodoButton.addEventListener("click", () => {
@@ -75,22 +69,16 @@ addTodoForm.addEventListener("submit", (evt) => {
   const name = evt.target.name.value;
   const dateInput = evt.target.date.value;
 
-  // Create a date object and adjust for timezone
+
   const date = new Date(dateInput);
   date.setMinutes(date.getMinutes() + date.getTimezoneOffset());
 
   const id = uuidv4();
   const values = { name, date, id };
-  const todo = generateTodo(values);
-  todosList.prepend(todo);
+  const todoElement = generateTodo(values);
+  section.addItem(todoElement);
   evt.target.reset();
   closeModal(addTodoPopup);
-});
-
-
-initialTodos.forEach((item) => {
-  const todo = generateTodo(item);
-  todosList.append(todo);
 });
 
 const newTodoValidator = new FormValidator(validationConfig, addTodoForm);
